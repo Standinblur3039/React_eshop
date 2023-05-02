ae60091621f40fa83f09c797ea01c3753b8f94a2
staninblur3039 <saitharun3039@gmail.com>
2023-05-02 15:12:00 +0530

    fix:Checkout_page

--- eshop/src/5CheckoutProduct_Comps/CheckoutProduct.jsx
+++ eshop/src/5CheckoutProduct_Comps/CheckoutProduct.jsx
@@ -4,12 +4,13 @@ import "./CheckoutProduct.css";
 function CheckoutProduct() {
   return (
     
-      <div className="checkoutproduct">
-        <img src = "https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt
-        className="checkoutPage__image" ClassName="checkout__ad" />
+      <div className="checkoutProduct">
+        <img src = "https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg" alt =""
+        className="checkoutProduct__image" />
 
-        <div className="product__info">
-          <p className="checkProduct__title">Bennett Mystic 15.6 inch Laptop Shoulder Messenger Sling Office Bag, Water Repellent Fabric for Men and Women (Blue)
+        <div className="checkoutProduct__info">
+          <p className="checkoutProduct__title">Bennett Mystic 15.6 inch Laptop Shoulder 
+          Messenger Sling Office Bag, Water Repellent Fabric for Men and Women (Blue)
           </p>
           <p className="checkoutProduct__price">
             <small>$</small>
--- eshop/src/6Subtotal_Comps/Subtotal.jsx
+++ eshop/src/6Subtotal_Comps/Subtotal.jsx
@@ -4,7 +4,7 @@ import CurrencyFormat from "react-currency-format";
 
 function Subtotal() {
   return (
-    <div className="Subtotal">
+    <div className="subtotal">
       <CurrencyFormat
         renderText={(value) => (
           <>
@@ -12,16 +12,16 @@ function Subtotal() {
               Subtotal (0 items): <strong>$0</strong>
             </p>
             <small className="subtotal__gift">
-              <input type="checkbox" /> This order contains a gift
+              <input type="checkbox"/> This order contains a gift 
             </small>
           </>
         )}
         decimalScale={2}
         value={0}
-        displayType="{text}"
+        displayType={"text"}
         thousandSeparator={true}
-        
       />
+      <button>Proceed to Checkout </button>
     </div>
   );
 }
